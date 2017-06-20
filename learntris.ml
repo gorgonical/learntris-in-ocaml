23aaa5b45856e6568685fae3f289f4c85944f104
Nick Gordon <nmgordon@memphis.edu>
2017-06-20 02:09:18 -0500

    Refactored board to be a matrix of tetramino.
    Refactoring done to prepare for printing rotated tetramino.  Refactored the
    board to be a matrix of tetramino.  This included some string-to-tetramino
    cell mappings et al.  Rotation was added as a record field, but is not
    implemented yet.  Some literals and magic numbers were removed, and more
    library stuff was used.

--- learntris.ml
+++ learntris.ml
@@ -1,6 +1,7 @@
 open Core;;
 
 type tetramino = I | O | Z | S | J | L | T | None;;
+type rotation = Left | Right | Mirrored | Normal;;
 
 let is_tetramino x =
   match x with
@@ -25,58 +26,67 @@ type game_info =
     mutable score : int;
     mutable lines_cleared : int;
     mutable active_tetramino : tetramino;
+    mutable tetramino_rotation : rotation;
   }
 ;;
 
+let cell_from_string string =
+  match string with
+  | "c" -> I
+  | "y" -> O
+  | "r" -> Z
+  | "g" -> S
+  | "b" -> J
+  | "o" -> L
+  | "m" -> T
+  | "." | _ -> None
+;;
+
 let read_board board =
-  (for i = 0 to 21 do
+  (for i = 0 to (Array.length board)-1 do
      let line = (match In_channel.input_line(In_channel.stdin) with
        | Some x -> x
        | None -> "")
      in
-     board.(i) <- Array.of_list (String.split line ~on:' ')
+     (* Split the line into a list of string tokens. Then map each
+        token to its tetramino counterpart and assign this row to that
+        map result. *)
+     board.(i) <- Array.map (Array.of_list (String.split line ~on:' '))
+                            ~f:(fun cell -> cell_from_string cell)
    done;
    board)
 ;;
 
-let clear_board () = [|
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-      [|".";".";".";".";".";".";".";".";".";"."|];
-  |]
+let clear_board () = Array.make_matrix ~dimx:22 ~dimy:10 (None : tetramino)
+;;
+
+
+
+let print_cell tetramino =
+  match tetramino with
+  | I -> "c"
+  | O -> "y"
+  | Z -> "r"
+  | S -> "g"
+  | J -> "b"
+  | L -> "o"
+  | T -> "m"
+  | None -> "."
 ;;
 
 let print_board board =
-  let num_rows = 22 in
-  let num_cols = 10 in
-  (for i = 0 to num_rows - 1 do
-     for j = 0 to num_cols - 1 do
-       Out_channel.output_string Out_channel.stdout (board.(i).(j) ^ (if j=num_cols-1 then ""
-                                                                      else " "))
-     done;
-     Out_channel.output_string Out_channel.stdout "\n";
-     Out_channel.flush Out_channel.stdout;
-   done;
-   board)
+  let num_cols = Array.length board.(0) in
+  begin
+  Array.iter board ~f:(fun row ->
+    begin
+      Array.iteri row ~f:(fun cindex cell ->
+                   Out_channel.output_string Out_channel.stdout
+                     (print_cell cell ^ if cindex = num_cols-1 then "" else " "));
+      Out_channel.output_string Out_channel.stdout "\n";
+      Out_channel.flush Out_channel.stdout;
+    end);
+  board
+  end
 ;;
 
 let process_query query_string board game_data =
@@ -98,42 +108,42 @@ let process_query query_string board game_data =
 
 let one_step board game_data =
   (Array.iter board ~f:(fun row ->
-       (if Array.for_all row ~f:(fun cell -> cell <> ".") then
-          (Array.fill row ~pos:0 ~len:(Array.length row) ".";
+       (if Array.for_all row ~f:(fun cell -> cell <> None) then
+          (Array.fill row ~pos:0 ~len:(Array.length row) None;
            (game_data.score <- game_data.score + 100;
             game_data.lines_cleared <- game_data.lines_cleared + 1))
        ));
    (board, game_data))
 ;;
 
-let print_tetramino tetramino =
+let print_tetramino tetramino_tuple =
   let tetramino_string =
-    (match tetramino with
-    | I ->
+    (match tetramino_tuple with
+    | (I, _) ->
         (". . . ." ^ "\n" ^
          "c c c c" ^ "\n" ^
          ". . . ." ^ "\n" ^
          ". . . ." ^ "\n")
-    | O ->
+    | (O, _) ->
         ("y y" ^ "\n" ^
          "y y")
-    | Z ->
+    | (Z, _) ->
         ("r r ." ^ "\n" ^
          ". r r" ^ "\n" ^
          ". . ." ^ "\n")
-    | S ->
+    | (S, _) ->
        (". g g" ^ "\n" ^
         "g g ." ^ "\n" ^
         ". . ." ^ "\n")
-    | J ->
+    | (J, _) ->
        ("b . ." ^ "\n" ^
         "b b b" ^ "\n" ^
         ". . ." ^ "\n")
-    | L ->
+    | (L, _) ->
        (". . o" ^ "\n" ^
         "o o o" ^ "\n" ^
         ". . ." ^ "\n")
-    | T ->
+    | (T, _) ->
        (". m ." ^ "\n" ^
         "m m m" ^ "\n" ^
         ". . ." ^ "\n")
@@ -188,7 +198,7 @@ let rec input_loop board game_data command_queue =
      input_loop new_board new_data command_queue
   | Some x when x = "t" ->
      begin
-       print_tetramino game_data.active_tetramino;
+       print_tetramino (game_data.active_tetramino, game_data.tetramino_rotation);
        input_loop board game_data command_queue
      end
   | Some x when is_tetramino x ->
@@ -203,5 +213,6 @@ let game_data = {
     score = 0;
     lines_cleared = 0;
     active_tetramino = None;
+    tetramino_rotation = Normal;
   } in
 input_loop (clear_board ()) game_data (Queue.create ());;
